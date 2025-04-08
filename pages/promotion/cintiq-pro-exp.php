a79783604c26e4bd4d7215a7b8de3d9b3aec3661
gonnajoong <gonnajoong@gmail.com>
2025-04-08 16:41:32 +0900

    신티크 프로 체험양식 페이지 변경
    스타일 추가 및 양식 문서 추가

--- pages/promotion/cintiq-pro-exp.php
+++ pages/promotion/cintiq-pro-exp.php
@@ -38,87 +38,15 @@
     <section class="hj-form-list-wrap">
         <div class="hj-form-main-wrap">
             <article class="hj-checklist-wrap">
-                <h2>데모장비 지원 요청서</h2>
                 <div class="hj-form-text">
-                    <img class="hj-promotion-title-image-1" src="../../images/banners/promotion_banner.png" alt="promotion_img">
+                    <img class="hj-promotion-title-image-1" src="../../images/banners/promotion_doc.jpg" alt="promotion_img">
                 </div>
-                <h2>고객정보</h2>
                 <div class="hj-form-normal-wrap">
-                    <form method="POST" id="emailForm" class="hj-form-wrap">
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="CompanyName" class="hj-form-label-title">신청 업체명</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="CompanyName" required>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="PersonName" class="hj-form-label-title">담당자</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="PersonName" required>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="PhoneNum" class="hj-form-label-title">연락처</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="PhoneNum" placeholder="010-0000-0000" required>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="EmailAddress" class="hj-form-label-title">이메일</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="EmailAddress" placeholder="email@test.com" requiredc>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label class="hj-form-label-title">데모 신청 장비</label>
-                            <span>
-                                <label for="CintiqPro_17">신티크 프로 17' SET</label><input class="hj-check-box hj-normal-checklist" type="checkbox" id="CintiqPro_17">
-                                <label for="CintiqPro_22">신티크 프로 22' SET</label><input class="hj-check-box hj-normal-checklist" type="checkbox" id="CintiqPro_22">
-                                <label for="CintiqPro_27">신티크 프로 27' SET</label><input class="hj-check-box hj-normal-checklist" type="checkbox" id="CintiqPro_27">
-                            </span>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="Purpose" class="hj-form-label-title">데모 신청 목적</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="Purpose" required>
-                        </span>
-                        <h2>장비 수령지 및 기타</h2>
-                        <span class="hj-form-label-style hj-form-normal-label hj-date-wrap">
-                            <label for="DayOfHope" class="hj-form-label-title">수령 희망일</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="DayOfHope" readonly required>
-                            <button id="DateInsert" class="hj-date-button" type="button">입력</button>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="Address" class="hj-form-label-title">수령지 주소</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="Address" required readonly>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="DetailAddress" class="hj-form-label-title">상세 주소</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="DetailAddress" required>
-                        </span>
-                        <span class="hj-form-label-style hj-form-normal-label">
-                            <label for="ReturnDay" class="hj-form-label-title">반납 예정일</label>
-                            <input type="text" class="hj-email-input hj-normal-form-input" id="ReturnDay" readonly required>
-                        </span>
-                        <div class="hj-promotion-warn">
-                            <h2>[ 참고 사항 ] 데모 장비 사용 및 반납관련</h2>
-                            <ul>
-                                <li>
-                                    <p>- <strong>반납 기한을 엄수</strong> 바랍니다. 반납기간 초과時 추후 지원에 제한이 있을 수 있습니다.</p>
-                                </li>
-                                 <li>
-                                    <p>- <strong>반납전</strong> 펜 및 Cable 등 각종 악세사리류 등의 누락이 없도록 확인 바랍니다.</p>
-                                 </li>
-                                 <li>
-                                    <p>- <strong>데모장비 Box에 매직펜 표기나 과도한 테이핑을 하지 않도록</strong> 부탁드립니다.</p>
-                                 </li>
-                                 <li>
-                                    <p>- 반납時 운송 중 장비 파손위험이 있으니, 수도권의 경우 가능한 방문 또는 "퀵"으로 반납 부탁드립니다.</p>
-                                 </li>
-                                 <li>
-                                    <p>- 데모장비 지원은 무상이나 <strong>반납時 운송비용은 고객/파트너사 부담</strong>이니 양해 바랍니다.</p>
-                                 </li>
-                            </ul>
-                        </div>
-                        
-                        <span class="hj-form-label-style button-box">
-                            <button class="hj-form-submit-button" id="FormSubmitButton" type="submit">신청하기</button>
-                        </span>
-                    </form>
-                <div class="hj-systems-form-image-1"></div>
+                    <span class="hj-form-label-style button-box">
+                         <a href="../../doc/wacom_promotion.docx" download><button class="hj-form-submit-button">요청서 다운로드 ( 워드 )</button></a>
+                        <a href="../../doc/wacom_promotion.hwp" download><button class="hj-form-submit-button">요청서 다운로드 ( 한글 )</button></a>
+                    </span>
                 </div>
-
             </article>
         </div>
     </section>
